48ca01a271f0a8047ee58cd195049748c1657f58
Georg Kunz <kwd@gmx.ch>
2011-06-08 19:29:41 +0200

    new Rails app generated by rails_apps_composer

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+gem 'rails', '3.1.0.rc1'
+gem 'sqlite3'
+gem 'sass'
+gem 'coffee-script'
+gem 'uglifier'
+gem 'jquery-rails'
+group :test do
+  
+  gem 'turn', :require => false
+end
+gem "haml", ">= 3.1.1"
+gem "haml-rails", ">= 0.3.4", :group => :development
+gem "devise", ">= 1.3.4"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyOnRailsCh::Application.config.secret_token = '83a65a6d81961f0cdd9cf0562ac00f39251a70236c32d05c7a7dfe179920f678214b60514fa0283ddc4f4dbba00b1c8aaee681c1cac430e1b9cbefd10b5553c1'
